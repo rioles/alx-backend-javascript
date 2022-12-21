4940576dba4a1f83dc8b7469208bfc632c09045c
rioles <rioles1992@gmail.com>
2022-12-21 11:41:39 +0100

    fix erroe

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     let task2 = true;
   
     if (trueOrFalse) {
-      const task = true; // eslint-disable-line no-unused-vars
-      const task2 = false; // eslint-disable-line no-unused-vars
+      task = true; // eslint-disable-line no-unused-vars
+      task2 = false; // eslint-disable-line no-unused-vars
     }
   
     return [task, task2];
