0ffa69dfd46bec8ea68855bb9839ed0861001d5a
rioles <rioles1992@gmail.com>
2023-01-03 17:59:58 +0100

    add gitignore

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+function getListStudents(){
+    const arrayStudent = []
+    arrayStudent[0] = {
+        'id': 1,
+        'firstName' : 'Guillaume',
+        'location' : 'San Francisco'
+    }
+    
+    arrayStudent[1] = {
+        'id': 2,
+        'firstName' : 'James',
+        'location' : 'Columbia'
+    }
+    
+    arrayStudent[2] = {
+        'id': 5,
+        'firstName' : 'Serena',
+        'location' : 'San Francisco'
+    }
+    
+   return arrayStudent
+}
