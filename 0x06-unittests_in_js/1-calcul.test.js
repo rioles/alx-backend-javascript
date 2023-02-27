08caf327ba104293cc6efd57de005309aa503f67
rioles <rioles1992@gmail.com>
2023-02-27 12:06:52 +0100

    basic test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,134 +1,36 @@
 const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  describe('type == "SUM"', () => {
-    it('equal positive numbers', () => {
-      assert.strictEqual(calculateNumber('SUM', 2.0, 2.0), 4);
-    });
-
-    it('equal positive numbers (alternate)', () => {
-      assert.strictEqual(calculateNumber('SUM', 2.3, 1.8), 4);
-    });
-
-    it('equal negative numbers', () => {
-      assert.strictEqual(calculateNumber('SUM', -2.0, -2.0), -4);
-    });
-
-    it('equal negative numbers (alternate)', () => {
-      assert.strictEqual(calculateNumber('SUM', -2.3, -1.8), -4);
-    });
-
-    it('negative and positive numbers', () => {
-      assert.strictEqual(calculateNumber('SUM', -2.0, 2.0), 0);
-    });
-
-    it('positive and negative numbers', () => {
-      assert.strictEqual(calculateNumber('SUM', 2.0, -2.0), 0);
-    });
-
-    it('0 and 0', () => {
-      assert.strictEqual(calculateNumber('SUM', 0.0, 0.0), 0);
-    });
+  it('floating point whole numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.0), 3);
   });
 
-  describe('type == "SUBTRACT"', () => {
-    it('equal positive numbers', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 2.0, 2.0), 0);
-    });
-
-    it('equal positive numbers (alternate)', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 2.3, 1.8), 0);
-    });
-
-    it('equal negative numbers', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', -2.0, -2.0), 0);
-    });
-
-    it('equal negative numbers (alternate)', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', -2.3, -1.8), 0);
-    });
-
-    it('negative and positive numbers', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', -2.0, 2.0), -4.0);
-    });
-
-    it('positive and negative numbers', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 2.0, -2.0), 4.0);
-    });
-
-    it('0 and 0', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 0.0, 0.0), 0);
-    });
+  it('rounding down b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.4), 3);
   });
 
-  describe('type == "DIVIDE"', () => {
-    it('positive numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 8.0, 2.0), 4.0);
-    });
+  it('rounding down a and b\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.4), 3);
+  });
 
-    it('numbers with different signs', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -7.0, 2.0), -3.5);
-    });
+  it('rounding down a\'s floating point fractional number', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+  });
 
-    it('numbers with different signs (alternate)', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 7.0, -2.0), -3.5);
-    });
+  it('rounding up b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
 
-    it('negative numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -7.0, -2.0), 3.5);
-    });
+  it('rounding up a and b\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.5), 6);
+  });
 
-    it('equal positive numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 2.0, 2.0), 1);
-    });
+  it('rounding up a\'s floating point fractional numbers', () => {
+    assert.strictEqual(calculateNumber(2.6, 2.0), 5);
+  });
 
-    it('equal negative numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -2.0, -2.0), 1);
-    });
-
-    it('equal rounded up numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 2.6, 3.0), 1);
-    });
-
-    it('equal rounded down numbers', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 2.4, 2.0), 1);
-    });
-
-    it('0 and positive number', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 0.0, 5.0), 0);
-    });
-
-    it('0 and negative number', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 0.0, -5.0), -0);
-    });
-
-    it('positive number and 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 5.0, 0), 'Error');
-    });
-
-    it('positive number and number rounded down to 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 5.0, 0.2), 'Error');
-    });
-
-    it('positive number and number rounded up to 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 5.0, -0.2), 'Error');
-    });
-
-    it('negative number and 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -5.0, 0), 'Error');
-    });
-
-    it('negative number and number rounded down to zero', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -5.0, 0.2), 'Error');
-    });
-
-    it('negative number and number rounded up to zero', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -5.0, -0.2), 'Error');
-    });
-
-    it('0 and 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 0.0, 0.0), 'Error');
-    });
+  it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
+    assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
   });
 });
